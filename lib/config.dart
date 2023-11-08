ccbee981c4796d1e4decfb9d030a1a55dc474b7b
ekos <heyrebornofficial@gmail.com>
2023-11-09 03:42:11 +0700

    Edits

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
-const String appName = "Cryptomask";
-const infuraKey = "67ab155e260d4fd1a25ff446399b81f5";
-const aboutUrl = "https://metamask.io/about/";
-const helpUrl = "https://metamask.zendesk.com/hc/en-us";
+const String appName = "RB Wallet";
+const infuraKey = "4617294cc76a423e856c2130e8d1dc8b";
+const aboutUrl = "https://re-born.company/about-us/";
+const helpUrl = "https://re-born.company/wallet-help";

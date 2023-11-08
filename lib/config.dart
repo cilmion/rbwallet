a857cc14a3b53c46e74a3e401ec245f2d31af13f
praveen vino <praveenvino@praveens-MacBook-Pro.local>
2023-11-08 19:16:14 +0530

    Source code added

---
+++ lib/config.dart
@@ -0,0 +1,4 @@
+const String appName = "Cryptomask";
+const infuraKey = "67ab155e260d4fd1a25ff446399b81f5";
+const aboutUrl = "https://metamask.io/about/";
+const helpUrl = "https://metamask.zendesk.com/hc/en-us";
